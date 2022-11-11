c238af941ceb0be9a54ec89964176b6b152cddb3
alanmfgoulart <alangoulart333@gmail.com>
2022-11-11 07:56:37 -0300

    commit inicial

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,196 @@
+<template>
+    <div id="burguer-table">
+        <Message :msg="msg" v-show="msg" />
+        <div>
+            <div id="burguer-table-heading">
+                <div class="order-id">#</div>
+                <div>Cliente:</div>
+                <div>Pão</div>
+                <div>Carne:</div>
+                <div>Opcionais</div>
+                <div>Ações:</div>
+            </div>
+        </div>
+        <div>
+            <div id="burguer-table-rows">
+                <div class="burguer-table-row" v-for="burguer in burguers" :key="burguer.id">
+                    <div class="order-number">{{ burguer.id }}</div>
+                    <div class="burguer-table-row-div">{{ burguer.nome }}</div>
+                    <div class="burguer-table-row-div">{{ burguer.pao }}</div>
+                    <div class="burguer-table-row-div">{{ burguer.carne }}</div>
+                    <div class="burguer-table-row-div">
+                        <ul>
+                            <li v-for="(opcional, index) in burguer.opcionais" :key="index">
+                                {{ opcional }}
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="burguer-table-row-div">
+                        <select name="status" class="status" @change="updateBurguer($event, burguer.id)">
+                            <!--<option value="">Selecione</option>-->
+                            <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burguer.status == s.tipo" >
+                                {{ s.tipo }}
+                            </option>
+                        </select>
+                        <button class="delete-btn" @click="deleteBurguer(burguer.id)">Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import Message from './Message.vue';
+
+
+    export default {
+        name: 'Dashboard',
+        data () {
+            return {
+                burguers: null,
+                burguers_id: null,
+                status: [],
+                msg: null
+            }
+        },
+        methods: {
+            async getPedidos() {
+
+                const req = await fetch("http://localhost:3000/burguers");
+                const data = await req.json();
+                this.burguers = data;
+
+                //resgatar o status
+
+                this.getStatus();
+
+            },
+            
+            async getStatus() {
+
+                const req = await fetch("http://localhost:3000/Status");
+                const data = await req.json();
+                this.status = data;
+
+            },
+
+            async deleteBurguer(id) {
+                
+                const req = await fetch('http://localhost:3000/burguers/'+id, {method: "DELETE"});                          
+                
+                const res = await req.json();
+
+                // msg
+                // colocar uma msg de sistema
+                this.msg = `Pedido removido com sucesso`
+
+                // limpar a mensagem
+
+                setTimeout(() => this.msg = "", 3000);
+                
+
+                this.getPedidos();
+
+            },
+
+            async updateBurguer(event, id) {
+
+                const option = event.target.value;
+
+                const dataJson = JSON.stringify({status: option});
+
+                const req = await fetch("http://localhost:3000/burguers/"+id, {
+                    method: "PATCH",
+                    headers: {"Content-Type": "application/json"},
+                    body: dataJson
+                });
+
+                const res = await req.json();
+
+                // colocar uma msg de sistema
+                this.msg = `Pedido Nº ${res.id} foi atualizado para ${res.status}`
+
+                // limpar a mensagem
+                setTimeout(() => this.msg = "", 3000);
+
+
+                //console.log(res);
+
+
+            }
+
+
+        },
+    
+        mounted() {
+            this.getPedidos();
+        },
+        components: {
+            Message
+        }
+    }
+</script>
+
+<style scoped>
+    #burguer-table {
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    #burguer-table-heading,
+    #burguer-table-rows,
+    .burguer-table-row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    #burguer-table-heading {
+        font-weight: bold;
+        padding: 12px;
+        border-bottom: 3px solid #333;
+    }
+
+    #burguer-table-heading div,
+    .burguer-table-row-div{
+        width: 19%;
+    }
+
+    .burguer-table-row{
+        width: 100%;
+        padding: 12px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    #burguer-table-heading .order-id,
+    .order-number {
+        width: 5%;
+    }
+
+    select {
+        padding: 12px 7px;
+        margin-right: 12px;
+    }
+
+    .delete-btn {
+        background-color: #222;
+        color: #fcba03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px 6px;
+        font-size: 18px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .delete-btn:hover {
+        background-color: transparent;
+        color: #222;
+    }
+    #burguer-tabela-row .order-number {
+        width: 5%;
+    }
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu Burguer:</h1>
+    <burguer-form />
+   
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue';
+import BurguerForm from '@/components/BurguerForm.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+    BurguerForm
+  }
+}
+
+
+</script>
